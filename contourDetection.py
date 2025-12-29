ad9a7a2df17e31f1a880791940a6fbbd842fca3a
Alish002 <ali.sh.helloworld@gmail.com>
2025-12-30 00:36:45 +0330

    Add night vision preprocessing step

--- contourDetection.py
+++ contourDetection.py
@@ -4,18 +4,16 @@
 import cv2
 import numpy as np
 
-
-
 image = cv2.imread('cirb.tif')
 assert image is not None, 'iamge not found !'
 
-img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #blur
-blur = cv2.GaussianBlur(img, (7,7), 0)
+img_gray = cv2.GaussianBlur(img_gray, (7,7), 0)
 
 #threshold
-ret, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
+ret, thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY)
 
 kernel = np.ones((9,9), np.uint8)
 
@@ -30,8 +28,12 @@ dilation = cv2.dilate(erosion, kernel, iterations=1)
 contours, hierarchy= cv2.findContours(image=dilation, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 print(f'contouts:{len(contours)}')
 
-imgCopy = img.copy()
+imgCopy = image.copy()
+
 cv2.drawContours(image=imgCopy, contours=contours, contourIdx=-1, color=(0,255,0),thickness=2, lineType=cv2.LINE_AA)
+
+cv2.putText(imgCopy, f"contouts:{len(contours)}", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+cv2.imwrite('contoures.jpg', imgCopy)
 
 cv2.imshow('erosion', erosion)
 cv2.imshow('dilation', dilation)
